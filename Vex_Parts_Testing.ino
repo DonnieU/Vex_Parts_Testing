897a36c465e10aa1d4f2dc63f2e988ab138dbad1
Donnie Utley <utley@slu.edu>
2016-10-23 15:34:07 -0500

    Added simple switch/case to test speeds and directions of motors

--- Vex_Parts_Testing.ino
+++ Vex_Parts_Testing.ino
@@ -1,34 +1,52 @@
 #include <Servo.h>
 
-#define LINETRACKER1 0  // A0
-#define POT1 1          // A1
-#define TRIGGER_PIN 11   // Digital pin 50
-#define ECHO_PIN 12   // Digital pin 52
+// pin (PWM) 13 used for motor
+#define LINETRACKER1 A0      // A0 in analog mode
+#define POT1 A1              // A1 in analog mode
+#define TRIGGER_PIN A4       // A4 in digital mode; used to "start" Ultrasonic module
+#define ECHO_PIN A3          // A3 in digital mode; used to determine time duration from Ultrasonic module
+#define DELAY_1_SEC 1000     // time delay used for motor controller, in ms
+#define DELAY_2_SEC 2000     // time delay used for motor controller, in ms
 
-Servo servo1;
-int val = 0;
-long duration = 0;
+Servo motor1;
+int val = 0;                          // temp variable used for sensors
+long duration = 0;                    // duration found from Ultrasonic module
+static unsigned long prevTime = 0;    // used in calculation of time lapse for motorTest
+static unsigned int state = 0;
 
-/*  void motorTest(void)
- *  VEX Motor Controller 29 with VEX 2-wire 393 Motor using Arduino Servo library:
+/*  
+ *  void motorTest(void)
+ *  VEX Motor Controller 29 with VEX 2-wire 393 Motor using Arduino Servo.h library:
  *  Full stop = 1500   
  *  Full speed forward = 2000
  *  Full speed reverse = 1000
- *  EX: servo1.write(2000) == motor turns full speed forward
+ *  EX: motor1.write(2000) == motor turns full speed forward
  */
 void motorTest(void) {
-  servo1.write(1500);   // Stop motor
-  delay(1000);
-  servo1.write(2000);   // Full speed ahead!
-  delay(2000);
-  servo1.write(1750);   // Half speed forward!
-  delay(2000);
-  servo1.write(1500);   // Stop
-  delay(1000);
-  servo1.write(1000);   // Full speed reverse
-  delay(2000);
-  servo1.write(1250);   // Half speed reverse
-  delay(2000);
+  unsigned long currTime = millis();
+
+ // check time and decide when to increment 
+  if ( (currTime - prevTime >= DELAY_1_SEC) && (!state || (state == 3)) ) { 
+    prevTime = currTime;
+    state++;
+  } else if (currTime - prevTime >= DELAY_2_SEC) { 
+    prevTime = currTime;
+    state++; 
+  }
+  
+  if (state > 5) { state = 0; } // start over
+  
+  // for testing just kept it simple by keeping track of current state and incrementing
+  switch (state) {
+    case 0: motor1.write(1500); break; // motor stop
+    case 1: motor1.write(2000); break; // Full speed ahead!
+    case 2: motor1.write(1750); break; // half speed forward
+    case 3: motor1.write(1500); break; // motor stop
+    case 4: motor1.write(1000); break; // full reverse speed
+    case 5: motor1.write(1250); break; // half speed reverse
+    default: motor1.write(1500); // motor stop
+  }
+ 
 } // end motorTest()
 
 /*
@@ -81,18 +99,18 @@ void setup() {
   Serial.begin(9600);
   Serial.println("");
   
-  servo1.attach(13);   // PWM 13
-  servo1.write(1500);  // stop motor by default
+  motor1.attach(13);   // PWM 13
+  motor1.write(1500);  // stop motor by default
 
   pinMode(TRIGGER_PIN, OUTPUT); // with reference to the INPUT on the Ultrasonic Module
-  pinMode(ECHO_PIN, INPUT); // with reference to the OUTPUT on the Ultrasonic Module
+  pinMode(ECHO_PIN, INPUT);     // with reference to the OUTPUT on the Ultrasonic Module
   
   Serial.println("Beginning parts and sensors tests...");
  
 } // end setup()
 
 void loop() {
-  //motorTest();
+  motorTest();
   lineTrackerTest();
   potTest();
   ultrasonicTest();
